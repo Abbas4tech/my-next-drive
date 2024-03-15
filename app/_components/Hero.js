45e8803d41586f992838f1ee45ea21a162347481
Abbas4tech <shaikhabbas54@gmail.com>
2024-03-16 03:48:58 +0530

    💄 update: complete banner styles and descriptions

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Constant from "../_utils/Constant";
 
 const Hero = () => {
   return (
@@ -10,21 +11,20 @@ const Hero = () => {
             <span className="text-primary">Share</span> your files in one place
           </h1>
 
-          <p className="mt-4 sm:text-xl/relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-            illo tenetur fuga ducimus numquam ea!
+          <p className="mt-4 sm:text-xl/relaxed text-gray-500">
+            {Constant.desc}
           </p>
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
               href="#"
             >
               Get Started
             </a>
 
             <a
-              className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+              className="block w-full rounded px-12 py-3 text-sm font-medium text-blue-600 shadow hover:text-blue-700 focus:outline-none focus:ring active:text-primary sm:w-auto"
               href="#"
             >
               Learn More
